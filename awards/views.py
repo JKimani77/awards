43582924f93144fa2f88f0cd78cb1999b5ad289d
JKimani77 <jkkimani77@gmail.com>
2021-05-30 14:02:40 +0300

    added authentication urls and views

--- awards/views.py
+++ awards/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render,redirect
 #from .forms import SignUpForm,LoginForm, ProfileForm, ProjectForm, RatingForm
 from django.contrib.auth import login,logout,authenticate
 from .models import Profile, Project, Review
-from rest_framework.response import Response
-from rest_framework.views import APIView
+#from rest_framework.response import Response
+#from rest_framework.views import APIView
 from .models import Project
-from .serializers import ProjectSerializer,
+#from .serializers import ProjectSerializer,
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome to the Awards')
+    projects = Project.objects.all()
+
+    return render(request, 'home.html',{'projects':projects})
 
 # Create your views here.
--- clone/urls.py
+++ clone/urls.py
@@ -20,5 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('awards.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
